37d1c6851d531b6d7e1bd3161a1b8345de0a96d2
Jackson Kelsch <jwkelsch@noctrl.edu>
2021-02-05 17:08:05 -0600

    adding all

---
+++ src/wordguessgame.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : wordguessgame.cpp
+// Author      : Jackson Kelsch
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "hello world";
+
+
+
+	return 0;
+}
